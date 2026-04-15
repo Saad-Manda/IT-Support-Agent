ae737d78a19167c026ea9b6f8a98a8d44e8b17bb
Saad-Manda <manda_2401ai28@iitp.ac.in>
2026-04-15 20:34:17 +0530

    added main

--- panel/config.py
+++ panel/config.py
@@ -1,6 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 
 
---
+++ panel/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import uvicorn
+from .database import test_connection
+from .routes.user_routes import router
+
+app = FastAPI(title="IT Admin Panel")
+
+@app.on_event("startup")
+async def startup():
+    is_connected = await test_connection()
+    if is_connected:
+        print("Successfully connected to the database.")
+    else:
+        print("Warning: Could not connect to the database.")
+
+app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run("panel.main:app", host="0.0.0.0", port=5001, reload=True)
